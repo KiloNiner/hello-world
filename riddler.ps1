fdd10e223bf680e7e1d2d34040a36d2a9dffda6c
KiloNiner <karsten@karstens-mbp.home>
2016-02-20 12:20:02 +0100

    testing

--- riddler.ps1
+++ riddler.ps1
@@ -6,18 +6,18 @@
 function Get-RiddlerAuthenticationToken
 {
     <#
-        .SYNOPSIS
-        Returns authentication token based on input credentials.
-        .DESCRIPTION
-        This function will return a Riddler.io authentication token given a set of valid credentials.
-        Invalid credentials will return an error.
-        .EXAMPLE
-        Get-RiddlerAuthenticationToken -email 'demo@example.com' -password 's3cr3t'
-        Retrieve a token, based off your credentials.
-        .PARAMETER email
-        Username for an account on Riddler.io
-        .PARAMETER password
-        Password for an account on Riddler.io
+            .SYNOPSIS
+            Returns authentication token based on input credentials.
+            .DESCRIPTION
+            This function will return a Riddler.io authentication token given a set of valid credentials.
+            Invalid credentials will return an error.
+            .EXAMPLE
+            Get-RiddlerAuthenticationToken -email 'demo@example.com' -password 's3cr3t'
+            Retrieve a token, based off your credentials.
+            .PARAMETER email
+            Username for an account on Riddler.io
+            .PARAMETER password
+            Password for an account on Riddler.io
     #>
     [CmdletBinding()]
     param
@@ -69,39 +69,39 @@ function Get-RiddlerAuthenticationToken
 function Get-RiddlerSearchResult
 {
     <#
-        .SYNOPSIS
-        Returns a resultset from Riddler.io, based off the input query.
-        .DESCRIPTION
-        This function will return a Riddler.io search response as an array of PSObjects.
-        Invalid authentication token will return an error.
-        .EXAMPLE
-        Get-RiddlerSearchResult -token 'WyI1NGE2ZjJhNjQyNmM3MDE4ZGE2N2YwZGQiLCJiMjcyYzBhZjhiMGZkMjJkMTZjNjkwZDg5NDFlMDVlMyJd.B7eMtg.1MncOII5jhL27U5owWZFV_Wr1yw' -query 'country:dk keyword:apache'
-        Perform a search for 'country:dk keyword:apache' using a manual token.
-        .EXAMPLE
-        Get-RiddlerSearchResult -token $RiddlerAuthenticationToken -query 'country:dk keyword:apache'
-        Perform a search for 'country:dk keyword:apache' using a token retrieved with the following (example) command:
-        $RiddlerAuthenticationToken = Get-RiddlerAuthenticationToken -email 'demo@example.com' -password 's3cr3t'
-        .EXAMPLE
-        Get-RiddlerSearchResult  -token $RiddlerAuthenticationToken -query 'country:dk keyword:apache' -limit 5 -output addr,cordinates,pld
-        Perform a search for 'country:dk keyword:apache', limited to 5 results and including the attributes addr,cordinates, and pld.
-        .PARAMETER token
-        A valid authentication token from Riddler.io, often retrieved via Get-RiddlerAuthenticationToken.
-        .PARAMETER query
-        A valid Riddler query. Query language is available at https://riddler.io/help/search.
-        Invalid queries will work as well, but are of limited use.
-        .PARAMETER limit
-        The maximum result set size to return. Actual result set may be smaller due to api limitations or search result set size.
-        .PARAMETER output
-        The attributes you wish returned, with defaults of addr and host.
-        Valid values are:
-        * addr         : the IP address of the host.
-        * applications : Headers and initial response from the host.
-        * cordinates   : GeoIP coordinates of the host.
-        * country_code : GeoIP country of the host.
-        * host         : FQDN of the host.
-        * keywords     : keywords associated with the host.
-        * pld          : Pay level domain.
-        * tld          : Top level domain.
+            .SYNOPSIS
+            Returns a resultset from Riddler.io, based off the input query.
+            .DESCRIPTION
+            This function will return a Riddler.io search response as an array of PSObjects.
+            Invalid authentication token will return an error.
+            .EXAMPLE
+            Get-RiddlerSearchResult -token 'WyI1NGE2ZjJhNjQyNmM3MDE4ZGE2N2YwZGQiLCJiMjcyYzBhZjhiMGZkMjJkMTZjNjkwZDg5NDFlMDVlMyJd.B7eMtg.1MncOII5jhL27U5owWZFV_Wr1yw' -query 'country:dk keyword:apache'
+            Perform a search for 'country:dk keyword:apache' using a manual token.
+            .EXAMPLE
+            Get-RiddlerSearchResult -token $RiddlerAuthenticationToken -query 'country:dk keyword:apache'
+            Perform a search for 'country:dk keyword:apache' using a token retrieved with the following (example) command:
+            $RiddlerAuthenticationToken = Get-RiddlerAuthenticationToken -email 'demo@example.com' -password 's3cr3t'
+            .EXAMPLE
+            Get-RiddlerSearchResult  -token $RiddlerAuthenticationToken -query 'country:dk keyword:apache' -limit 5 -output addr,cordinates,pld
+            Perform a search for 'country:dk keyword:apache', limited to 5 results and including the attributes addr,cordinates, and pld.
+            .PARAMETER token
+            A valid authentication token from Riddler.io, often retrieved via Get-RiddlerAuthenticationToken.
+            .PARAMETER query
+            A valid Riddler query. Query language is available at https://riddler.io/help/search.
+            Invalid queries will work as well, but are of limited use.
+            .PARAMETER limit
+            The maximum result set size to return. Actual result set may be smaller due to api limitations or search result set size.
+            .PARAMETER output
+            The attributes you wish returned, with defaults of addr and host.
+            Valid values are:
+            * addr         : the IP address of the host.
+            * applications : Headers and initial response from the host.
+            * cordinates   : GeoIP coordinates of the host.
+            * country_code : GeoIP country of the host.
+            * host         : FQDN of the host.
+            * keywords     : keywords associated with the host.
+            * pld          : Pay level domain.
+            * tld          : Top level domain.
 
     #>
 
