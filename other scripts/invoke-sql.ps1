1ac19db644dcf313345f04599515ded8e99e347e
KiloNiner <karsten@karstens-mbp.home>
2016-02-20 13:50:19 +0100

    More reorg and added script

---
+++ other scripts/invoke-sql.ps1
@@ -0,0 +1,24 @@
+# Found on stack exchange, allows you to perform a query against a MSSQL database with no additional dependencies.
+function Invoke-SQL 
+{
+    param(
+        [string] $dataSource = '.\SQLEXPRESS',
+        [string] $database = 'MasterData',
+        [string] $sqlCommand = $(throw 'Please specify a query.')
+    )
+
+    $connectionString = "Data Source=$dataSource; " +
+    'Integrated Security=SSPI; ' +
+    "Initial Catalog=$database"
+
+    $connection = New-Object -TypeName system.data.SqlClient.SQLConnection -ArgumentList ($connectionString)
+    $command = New-Object -TypeName system.data.sqlclient.sqlcommand -ArgumentList ($sqlCommand, $connection)
+    $connection.Open()
+
+    $adapter = New-Object -TypeName System.Data.sqlclient.sqlDataAdapter -ArgumentList $command
+    $dataset = New-Object -TypeName System.Data.DataSet
+    $null = $adapter.Fill($dataset)
+
+    $connection.Close()
+    $dataset.Tables
+}
